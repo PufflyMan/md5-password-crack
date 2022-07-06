aad4a4aa8a839257e9795dd34151c18791ab0368
Puffy <88508824+PufflyMan@users.noreply.github.com>
2022-07-06 14:30:33 +0400

    improved and bugged

--- index.php
+++ index.php
@@ -14,16 +14,47 @@
 
             for($i=0; $i<strlen($num); $i++ ) {
                 $ch1 = $num[$i];
-                print "$ch1";
                 
                 for($j=0; $j<strlen($num); $i++ ) {
                     $ch2 = $num[$j];
+
+                    for ($k=0; $k < strlen($num); $k++ ) {
+                        $ch3 = $num[$k];
+
+                        for ($r=0; $r < strlen($num); $r++) {
+                            $ch4 = $num[$r];
+                        }
+                    }
                 }
 
-                $try = $ch1.$ch2;
+                $try1 = $ch1.$ch2;
+                $$try2 = $$ch3.$ch4;
+                $try = $try1.$try2;
 
                 $check = hash('md5', $try);
+
+                if ($check == $md5) {
+                    $goodtext = $try;
+                    break;
+                }
+
+                if ($show > 0) {
+                    print "$check $try \n";
+                    $show = $show - 1;
+                }
             }
+
+            $time_post = microtime(true);
+            print "Elapsed time: ";
+            print $time_post - $time_pre;
+            print "\n";
         }
     ?>
 </pre>
+<p>
+    PIN: <?= htmlentities($goodtext); ?>
+</p>
+<form>
+    <input type="text" name="md5" size="60" />
+    <input type="submit" value="Crack MD5" />
+</form>
